9a02d3b144de32c710417b932bda11d981ea65e8
KamelaJda <kamilbydpol@gmail.com>
2022-06-22 15:47:49 +0200

    🐛 Fixed subscriptions deletion

--- src/main/java/me/kamelajda/services/SpotifyService.java
+++ src/main/java/me/kamelajda/services/SpotifyService.java
@@ -21,9 +21,12 @@ package me.kamelajda.services;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import me.kamelajda.jpa.models.ArtistCreation;
 import me.kamelajda.jpa.models.ArtistInfo;
 import me.kamelajda.jpa.models.GuildConfig;
 import me.kamelajda.jpa.models.UserConfig;
+import me.kamelajda.jpa.repository.ArtistCreationRepository;
+import me.kamelajda.utils.enums.CreationType;
 import me.kamelajda.utils.language.Language;
 import me.kamelajda.utils.language.LanguageService;
 import me.kamelajda.utils.language.LanguageType;
@@ -44,6 +47,8 @@ import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.*;
@@ -51,6 +56,7 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @Service
+
 public class SpotifyService {
 
     private final ScheduledExecutorService accessTokenScheduler = Executors.newScheduledThreadPool(1);
@@ -61,17 +67,17 @@ public class SpotifyService {
     private final ExecutorService sendMessagesExecutor = Executors.newFixedThreadPool(10);
 
     private final SpotifyApi api;
-    private final UserConfigService userConfigService;
     private final SubscribeArtistService subscribeArtistService;
     private final LanguageService languageService;
+    private final ArtistCreationRepository artistCreationRepository;
 
     @Getter @Setter private ShardManager shardManager;
 
-    public SpotifyService(SpotifyApi api, UserConfigService userConfigService, SubscribeArtistService subscribeArtistService, LanguageService languageService) {
+    public SpotifyService(SpotifyApi api, SubscribeArtistService subscribeArtistService, LanguageService languageService, ArtistCreationRepository artistCreationRepository) {
         this.api = api;
-        this.userConfigService = userConfigService;
         this.subscribeArtistService = subscribeArtistService;
         this.languageService = languageService;
+        this.artistCreationRepository = artistCreationRepository;
 
         refreshAccessToken();
         setupNotification();
@@ -81,11 +87,26 @@ public class SpotifyService {
         return api.searchArtists(query).limit(10).build().executeAsync();
     }
 
-    public Paging<AlbumSimplified> getLastAlbum(String artistId) throws IOException, ParseException, SpotifyWebApiException {
-        return api.getArtistsAlbums(artistId).offset(0).limit(1).build().execute();
+    public AlbumSimplified getLastAlbum(String artistId) throws IOException, ParseException, SpotifyWebApiException {
+        AlbumSimplified[] execute = api.getArtistsAlbums(artistId).offset(0).limit(1).build().execute().getItems();
+        if (execute == null || execute.length == 0) return null;
+        return execute[0];
     }
 
-    private void refreshAccessToken() {
+    public AlbumSimplified getLastTrack(String artistId) throws IOException, ParseException, SpotifyWebApiException {
+        AlbumSimplified[] execute = api.getArtistsAlbums(artistId).album_type("single").limit(1).offset(0).build().execute().getItems();
+        if (execute == null || execute.length == 0) return null;
+        return execute[0];
+    }
+
+    public AlbumSimplified getLastFeat(String artistId) throws IOException, ParseException, SpotifyWebApiException {
+        AlbumSimplified[] execute = api.getArtistsAlbums(artistId).album_type("appears_on,single").limit(10).offset(0).build().execute().getItems();
+        if (execute == null || execute.length == 0) return null;
+
+        return Arrays.stream(execute).filter(p -> !p.getArtists()[0].getId().equals(artistId)).findFirst().orElse(null);
+    }
+
+    public void refreshAccessToken() {
         try {
             ClientCredentials cr = api.clientCredentials().build().execute();
             api.setAccessToken(cr.getAccessToken());
@@ -94,8 +115,8 @@ public class SpotifyService {
             accessTokenScheduler.schedule(this::refreshAccessToken, 60, TimeUnit.SECONDS);
         }
     }
-
-    private long timeToRefresh(LanguageType lang) {
+    
+    public long timeToRefresh(LanguageType lang) {
         ZonedDateTime now = ZonedDateTime.now(lang.getTimeZone());
         ZonedDateTime nextRun = now.withHour(0).withMinute(10).withSecond(0);
 
@@ -104,11 +125,10 @@ public class SpotifyService {
         return Duration.between(now, nextRun).getSeconds();
     }
 
-    private void setupNotification() {
+    public void setupNotification() {
         checkAlbumsScheduler.scheduleAtFixedRate(
             () -> {
-                List<UserConfig> list = userConfigService.getAll();
-                Set<ArtistInfo> infos = subscribeArtistService.getAllArtist(list.stream().map(UserConfig::getUserId).collect(Collectors.toSet()));
+                Set<ArtistInfo> infos = subscribeArtistService.loadAll();
 
                 int index = 0;
 
@@ -116,25 +136,36 @@ public class SpotifyService {
 
                 for (ArtistInfo info : infos) {
                     executor.execute(() -> {
+                        List<Object[]> newCreations = new ArrayList<>();
+
                         try {
-                            AlbumSimplified[] album = getLastAlbum(info.getSpotifyId()).getItems();
-                            if (album.length == 0) return;
+                            configure(info.getLastAlbum(), getLastAlbum(info.getSpotifyId()), CreationType.ALBUM, info, newCreations);
+                        } catch (Exception e) {
+                            log.error("An error occurred while getting a last album", e);
+                        }
 
-                            AlbumSimplified newAlbum = album[0];
+                        try {
+                            configure(info.getLastTrack(), getLastTrack(info.getSpotifyId()), CreationType.TRACK, info, newCreations);
+                        } catch (Exception e) {
+                            log.error("An error occurred while getting a last track", e);
+                        }
 
-                            if (newAlbum.getExternalUrls().get("spotify").equals(info.getLastAlbumLink())) {
-                                return;
-                            }
+                        try {
+                            configure(info.getLastFeat(), getLastFeat(info.getSpotifyId()), CreationType.FEAT, info, newCreations);
+                        } catch (Exception e) {
+                            log.error("An error occurred while getting a last track", e);
+                        }
 
-                            info.setLastAlbumDate(newAlbum.getReleaseDate());
-                            info.setLastAlbumLink(newAlbum.getExternalUrls().get("spotify"));
-                            info.setLastAlbumName(newAlbum.getName());
+                        if (newCreations.isEmpty()) return;
 
+                        try {
                             subscribeArtistService.save(info);
 
                             for (GuildConfig guild : info.getSubscribeGuilds().stream().filter(f -> f.getNotificationChannelId() != null).collect(Collectors.toSet())) {
                                 sendMessagesExecutor.execute(() -> {
                                     Language l = languageService.get(guild.getLanguage());
+
+                                    List<MessageEmbed> embeds = embeds(l, info, newCreations, avatarUrl);
 
                                     try {
                                         Guild guildImpl = shardManager.getGuildById(guild.getGuildId());
@@ -148,7 +179,7 @@ public class SpotifyService {
                                             return;
                                         }
 
-                                        channel.sendMessageEmbeds(embed(l, info, newAlbum, avatarUrl)).queue();
+                                        channel.sendMessageEmbeds(embeds).queue();
                                     } catch (Exception ignored) { }
                                 });
                             }
@@ -156,11 +187,15 @@ public class SpotifyService {
                             for (UserConfig user : info.getSubscribeUsers()) {
                                 sendMessagesExecutor.execute(() -> {
                                     Language l = languageService.get(user.getLanguageType());
+                                    List<MessageEmbed> embeds = embeds(l, info, newCreations, avatarUrl);
+
                                     try {
                                         User u = getShardManager().retrieveUserById(user.getUserId()).complete();
                                         PrivateChannel channel = u.openPrivateChannel().complete();
-                                        channel.sendMessageEmbeds(embed(l, info, newAlbum, avatarUrl)).complete();
-                                    } catch (Exception ignored) { }
+                                        channel.sendMessageEmbeds(embeds).complete();
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
                                 });
                             }
 
@@ -176,19 +211,65 @@ public class SpotifyService {
                         } catch (InterruptedException ignored) { }
                     }
                 }
-            }, timeToRefresh(LanguageType.POLISH), TimeUnit.HOURS.toSeconds(5), TimeUnit.SECONDS
+//            }, timeToRefresh(LanguageType.POLISH), TimeUnit.HOURS.toSeconds(5), TimeUnit.SECONDS
+            }, 4, TimeUnit.HOURS.toSeconds(5), TimeUnit.SECONDS
         );
     }
 
-    private MessageEmbed embed(Language l, ArtistInfo info, AlbumSimplified newAlbum, String avatarUrl) {
+    public ArtistCreation isNew(ArtistCreation old, AlbumSimplified maybeNew, CreationType creationType) {
+        if (old.getLink().equals(maybeNew.getExternalUrls().get("spotify"))) return null;
+
+        return artistCreationRepository.save(SubscribeArtistService.createCreation(creationType, maybeNew));
+    }
+
+    public boolean configure(ArtistCreation old, AlbumSimplified maybeNew, CreationType creationType, ArtistInfo info, List<Object[]> list) {
+        if (maybeNew == null) return false;
+        try {
+            ArtistCreation isNew = isNew(old, maybeNew, creationType);
+            if (isNew == null) return false;
+
+            switch (creationType) {
+                case ALBUM:
+                    info.setLastAlbum(isNew);
+                    break;
+                case TRACK:
+                    info.setLastTrack(isNew);
+                    break;
+                case FEAT:
+                    info.setLastFeat(isNew);
+                    break;
+                default: log.error("CreationType {} is invalid!", creationType);
+            }
+
+            list.add(new Object[] {maybeNew, isNew});
+            return true;
+        } catch (Exception e) {
+            log.error("An error occurred while getting a last album", e);
+        }
+
+        return false;
+    }
+
+    public List<MessageEmbed> embeds(Language l, ArtistInfo info, List<Object[]> objects, String avatarUrl) {
+        List<MessageEmbed> embeds = new ArrayList<>();
+
+        for (Object[] object : objects) {
+            embeds.add(embed(l, info, (AlbumSimplified) object[0], avatarUrl, (ArtistCreation) object[1]));
+        }
+
+        return embeds;
+    }
+
+    public MessageEmbed embed(Language l, ArtistInfo info, AlbumSimplified newAlbum, String avatarUrl, ArtistCreation creation) {
         EmbedBuilder eb = new EmbedBuilder();
         eb.setColor(Color.BLUE);
-        eb.setTitle(l.get("spotify.service.notification.new.album"));
+        eb.setTitle(l.get("spotify.service.notification.new." + creation.getCreationType().name().toLowerCase()));
         eb.setDescription(l.get("spotify.service.notification.description",
+            l.get("spotify.service.notification.type." + creation.getCreationType().name().toLowerCase()),
             info.getDisplayName(),
             info.getLink(),
-            info.getLastAlbumName(),
-            info.getLastAlbumLink()));
+            newAlbum.getName(),
+            newAlbum.getExternalUrls().get("spotify")));
 
         if (newAlbum.getImages().length > 0) eb.setImage(newAlbum.getImages()[0].getUrl());
 
